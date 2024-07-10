f93d28c1e53f8a23836e5670765f5d238b0f106e
Adelmo Menezes - Debian <adelmo.msp@gmail.com>
2024-07-10 08:06:40 -0300

    Criação Participant.java 20:00min

---
+++ src/main/java/br/com/dheldev/planner/participant/Participant.java
@@ -0,0 +1,10 @@
+package br.com.dheldev.planner.participant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "participants")
+public class Participant {
+    
+}
