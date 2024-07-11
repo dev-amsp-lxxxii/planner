a44e1cdc6daf2553b793865fb5f13025b46be40a
Adelmo Menezes - Debian <adelmo.msp@gmail.com>
2024-07-10 23:45:17 -0300

    Criar repository da entidade atividades

--- src/main/java/br/com/dheldev/planner/activities/ActivityRepository.java
+++ src/main/java/br/com/dheldev/planner/activities/ActivityRepository.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package br.com.dheldev.planner.activities;
 
-/**
- *
- * @author adelmo
- */
 public interface ActivityRepository {
     
 }
