95938667f4dacc2440d891b826e95ef4732ac3cf
Adelmo Menezes - Debian <adelmo.msp@gmail.com>
2024-07-10 23:41:43 -0300

    Criar entidade que irá representar uma Activity

---
+++ src/main/java/br/com/dheldev/planner/activities/Activity.java
@@ -0,0 +1,37 @@
+package br.com.dheldev.planner.activities;
+
+import br.com.dheldev.planner.trip.Trip;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "activities")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+    
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
